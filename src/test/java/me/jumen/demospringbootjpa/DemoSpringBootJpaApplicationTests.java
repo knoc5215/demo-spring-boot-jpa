5e4e1972be4fc7a4ddab3e626ae5dba856a11a45
knoc5215 <knoc5215@naver.com>
2020-12-24 15:55:42 +0900

    스프링 데이터 5부: 스프링 데이터 JPA 소개 - Initial commit

---
+++ src/test/java/me/jumen/demospringbootjpa/DemoSpringBootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jumen.demospringbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
